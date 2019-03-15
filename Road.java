d6cf36a76bc9177728ca4f516a941e2409ea5fb7
kavyar1999 <36255985+kavyar1999@users.noreply.github.com>
2019-03-15 21:48:31 +0530

    Update Road.java

--- Road.java
+++ Road.java
@@ -7,7 +7,7 @@ class Sensor
   File file = new File("https://github.com/kavyar1999/Pothole_Detection/blob/master/gps.txt"); 
   BufferedReader rdr = new BufferedReader(new FileReader(file)); 
   String st; 	
-  PrintStream o = new PrintStream(new File("C:\\Users\\Jishnu\\Desktop\\pwd.txt"));
+  PrintStream o = new PrintStream(new File("https://github.com/kavyar1999/Pothole_Detection/blob/master/pwd.txt"));
   System.setOut(o); 
   while ((st = rdr.readLine()) != null)    				 //read from input file
   {  
